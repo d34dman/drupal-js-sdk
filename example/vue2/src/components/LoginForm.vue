28c8abdf99c008c6192a6ea90c79956ac411c2f2
David Berri <dwbwill@gmail.com>
2021-05-11 06:17:16 -0300

    Implements the Vue.js example

---
+++ example/vue2/src/components/LoginForm.vue
@@ -0,0 +1,178 @@
+<template>
+	<button v-if="user.loggedIn" @click="logout">Log out</button>
+	<form v-else @submit.prevent="submit" class="mt-8 space-y-6" action="#" method="POST">
+		<input type="hidden" name="remember" value="true" />
+		<div class="rounded-md shadow-sm -space-y-px">
+			<div>
+				<label for="username" class="sr-only">Username</label>
+				<input
+					:value="user.username"
+					id="username"
+					name="username"
+					type="text"
+					autocomplete="email"
+					required
+					class="
+						appearance-none
+						rounded-none
+						relative
+						block
+						w-full
+						px-3
+						py-2
+						border border-gray-300
+						placeholder-gray-500
+						text-gray-900
+						rounded-t-md
+						focus:outline-none
+						focus:ring-indigo-500
+						focus:border-indigo-500
+						focus:z-10
+						sm:text-sm
+					"
+					placeholder="Username"
+				/>
+			</div>
+			<div>
+				<label for="password" class="sr-only">Password</label>
+				<input
+					:value="user.password"
+					id="password"
+					name="password"
+					type="password"
+					autocomplete="current-password"
+					required
+					class="
+						appearance-none
+						rounded-none
+						relative
+						block
+						w-full
+						px-3
+						py-2
+						border border-gray-300
+						placeholder-gray-500
+						text-gray-900
+						rounded-b-md
+						focus:outline-none
+						focus:ring-indigo-500
+						focus:border-indigo-500
+						focus:z-10
+						sm:text-sm
+					"
+					placeholder="Password"
+				/>
+			</div>
+		</div>
+
+		<div class="flex items-center justify-between">
+			<div class="flex items-center">
+				<input
+					id="remember_me"
+					name="remember_me"
+					type="checkbox"
+					class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+				/>
+				<label for="remember_me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
+			</div>
+
+			<div class="text-sm">
+				<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+					Forgot your password?
+				</a>
+			</div>
+		</div>
+
+		<div>
+			<button
+				:disabled="inProgress"
+				type="submit"
+				class="
+					group
+					relative
+					w-full
+					flex
+					justify-center
+					py-2
+					px-4
+					border border-transparent
+					text-sm
+					font-medium
+					rounded-md
+					text-white
+					bg-indigo-600
+					hover:bg-indigo-700
+					focus:outline-none
+					focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+				"
+			>
+				<span class="absolute left-0 inset-y-0 flex items-center pl-3">
+					<!-- Heroicon name: solid/lock-closed -->
+					<svg
+						class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						aria-hidden="true"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</span>
+				Sign in
+			</button>
+		</div>
+	</form>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Drupal, DrupalAuth } from 'drupal-js-sdk';
+
+export default Vue.extend({
+	data() {
+		return {
+			user: {
+				username: '',
+				password: '',
+				loggedIn: false
+			},
+			inProgress: false,
+			error: false,
+			auth: {} as DrupalAuth
+		};
+	},
+	methods: {
+		submit() {
+			this.inProgress = true;
+			this.auth
+				.login(this.user.username, this.user.password)
+				.then((response) => {
+					this.inProgress = false;
+					this.user.username = response.data.current_user.name;
+					this.user.loggedIn = true;
+					this.user.password = '';
+				})
+				.catch((err) => {
+					this.error = err.response.data.message;
+				});
+		},
+		logout() {
+			this.auth.logout().then(() => {
+				this.user.loggedIn = false;
+				this.user.username = '';
+				this.user.password = '';
+			});
+		}
+	},
+	mounted() {
+		const drupal = new Drupal().initialize({
+			baseURL: process.env.VUE_APP_BASE_URL
+		});
+		this.auth = new DrupalAuth(drupal);
+	}
+});
+</script>
---
+++ example/vue2/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="home">
+		<nav>
+			<router-link to="/login">Login</router-link>
+		</nav>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'Home'
+});
+</script>
