7ade1dfe2fffed1b900876e6d4afc59911281b47
D34dMan <shibinkidd@gmail.com>
2021-05-30 19:59:09 +0200

    Add test for getErrorCode

--- src/__tests__/DrupalError.test.ts
+++ src/__tests__/DrupalError.test.ts
@@ -3,4 +3,5 @@ import {DrupalError} from '../DrupalError';
 test('Drupal initialize', () => {
   const error = new DrupalError(42, 'Solution');
   expect(error.toString()).toBe('DrupalError: 42 Solution');
+  expect(error.getErrorCode()).toBe(42);
 });
