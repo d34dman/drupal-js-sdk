7c8dbad39e2882fa95784db4a7e909399cf495ce
D34dMan <shibinkidd@gmail.com>
2022-01-05 19:34:20 +0100

    Remove Object.assign(moduel.exports ... hack

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,27 +1,8 @@
-import {Core} from './Core';
-import {Drupal} from './Drupal';
-import {DrupalAuth} from './DrupalAuth';
-import {DrupalError} from './DrupalError';
-import {DrupalMenu} from './DrupalMenu';
-import {DrupalRole} from './DrupalRole';
-import {DrupalSettings} from './DrupalSettings';
-import {AxiosClient} from './AxiosClient';
-
-
-export { Core };
-export { Drupal };
-export { DrupalAuth };
-export { DrupalError };
-export { DrupalMenu };
-export { DrupalRole };
-export { DrupalSettings };
-export { AxiosClient };
-
-Object.assign(module.exports, Core);
-Object.assign(module.exports, Drupal);
-Object.assign(module.exports, DrupalAuth);
-Object.assign(module.exports, DrupalError);
-Object.assign(module.exports, DrupalMenu);
-Object.assign(module.exports, DrupalRole);
-Object.assign(module.exports, DrupalSettings);
-Object.assign(module.exports, AxiosClient);
+export {Core} from './Core';
+export {Drupal} from './Drupal';
+export {DrupalAuth} from './DrupalAuth';
+export {DrupalError} from './DrupalError';
+export {DrupalMenu} from './DrupalMenu';
+export {DrupalRole} from './DrupalRole';
+export {DrupalSettings} from './DrupalSettings';
+export {AxiosClient} from './AxiosClient';
