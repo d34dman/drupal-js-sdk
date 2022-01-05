465969857211cd14e08ea77129de81bbb3e46724
D34dMan <shibinkidd@gmail.com>
2022-01-05 08:39:51 +0100

    Remove couple of test from drupal-js-sdk

--- packages/drupal-js-sdk/src/__tests__/Drupal.test.ts
+++ packages/drupal-js-sdk/src/__tests__/Drupal.test.ts
@@ -10,32 +10,3 @@ test('Drupal initialize', () => {
   expect(sdk.config.getItem('FOO')).toBe('bar');
   expect(sdk.config.getItem('baseURL')).toBe(config.baseURL);
 });
-
-test('Drupal initialize with headers', () => {
-  const config = {
-    baseURL: 'http://www.example.com',
-    headers: {
-      'X-Foo': 'Foo',
-      'X-Bar': {foo: 'bar'},
-      'X-Baz': true,
-    },
-  };
-  const sdk = new Drupal(config);
-  expect(sdk.initialize(config)).toBe(sdk);
-  sdk.config.setItem('FOO', 'bar');
-  expect(sdk.config.getItem('FOO')).toBe('bar');
-});
-
-test('Drupal initialize with basic auth', () => {
-  const config = {
-    baseURL: 'http://www.example.com',
-    auth: {
-      username: 'dpa',
-      password: 'dpa',
-    },
-  };
-  const sdk = new Drupal(config);
-  expect(sdk.initialize(config)).toBe(sdk);
-  sdk.config.setItem('FOO', 'bar');
-  expect(sdk.config.getItem('FOO')).toBe('bar');
-});
