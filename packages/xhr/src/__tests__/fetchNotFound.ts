2e6c3bb3c868ef81e0219dd859a526531b1aa18a
D34dMan <shibinkidd@gmail.com>
2022-12-29 13:26:15 +0100

    fix: fix fetchNotFound test for when fetch is not defined

--- packages/xhr/src/__tests__/fetchNotFound.ts
+++ packages/xhr/src/__tests__/fetchNotFound.ts
@@ -4,7 +4,7 @@ test('FetchClient throws error when fetch is not available', async () => {
   const config = {
     baseURL: 'https://drupal-js-sdk-demo.d34dman.com',
   };
-  if (fetch === undefined) {
+  if (typeof fetch === 'undefined') {
     expect(() => {new FetchClient(config);}).toThrow();
   }
 });
